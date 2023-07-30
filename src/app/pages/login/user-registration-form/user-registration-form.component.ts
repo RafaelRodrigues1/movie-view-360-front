73d91f391023b0f87153f6a04a755bad5d5921fe
Rafael Rodrigues da Silva <rafaelx7t@hotmail.com>
2023-07-30 17:11:36 -0300

    add func saveUser

--- src/app/pages/login/user-registration-form/user-registration-form.component.ts
+++ src/app/pages/login/user-registration-form/user-registration-form.component.ts
@@ -1,5 +1,7 @@
+import { UserClient } from './../../../core/clients/user.client';
 import { Component, AfterViewChecked } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { User } from 'src/app/shared/models/user';
 
 @Component({
   selector: 'app-user-registration-form',
@@ -13,8 +15,23 @@ export class UserRegistrationFormComponent implements AfterViewChecked {
   userEmail = new FormControl('', [Validators.required, Validators.email]);
   userPassword = new FormControl('', [Validators.required, Validators.minLength(this.minLengthPassword)])
 
+  constructor(private userClient: UserClient){}
+
   ngAfterViewChecked(): void {
     this.userPassword.addValidators(Validators.minLength(this.minLengthPassword))
+  }
+
+  saveUser() {
+    const user: User = this.buildUser()
+    this.userClient.saveClient(user).subscribe(user => console.log(user))
+  }
+
+  private buildUser(): User {
+    return {
+      name: this.userName.value!,
+      email: this.userEmail.value!,
+      password: this.userPassword.value!
+    }
   }
 
   hasFormControllError(): Boolean {
